3f70227f6f5aa9f87aaf4f7362685d5761d328df
Sonam <sonurauniyar2062@gmail.com>
2025-11-21 19:38:35 +0545

    assignment completed

--- src/q15.c
+++ src/q15.c
@@ -1,1 +1,31 @@
 // Write a function named countOccurrences that takes a string and a character as input and returns the number of times the character appears in the string.
+#include <stdio.h>
+
+int countOccurrences(char str[], char ch) 
+{
+    int count = 0;
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) 
+    {
+        if (str[i] == ch) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() 
+{
+    char str[1000], ch;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter the character to count: ");
+    scanf("%c", &ch);
+
+    int occurrences = countOccurrences(str, ch);
+    printf("Character '%c' occurs %d times in the string.\n", ch, occurrences);
+
+    return 0;
+}
