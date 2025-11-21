7043d9c39114104ae3f6ad4f379e14d4cc5ce61f
Sonam <sonurauniyar2062@gmail.com>
2025-11-21 19:46:20 +0545

    assignment completed

--- src/q17.c
+++ src/q17.c
@@ -1,1 +1,36 @@
 // Write a function named calculatePower that takes two integers, base and exponent, as input and returns the result of raising the base to the exponent.
+#include <stdio.h>
+
+long long calculatePower(int base, int exponent) 
+{
+    long long result = 1;
+    if (exponent < 0) 
+    {
+        printf("Negative exponent not supported.\n");
+        return -1;
+    }
+    for (int i = 0; i < exponent; i++) 
+    {
+        result *= base;
+    }
+    return result;
+}
+
+int main() 
+{
+    int base, exponent;
+
+    printf("Enter base: ");
+    scanf("%d", &base);
+
+    printf("Enter exponent: ");
+    scanf("%d", &exponent);
+
+    long long power = calculatePower(base, exponent);
+
+    if (power != -1) 
+    {
+        printf("%d raised to the power %d is %lld\n", base, exponent, power);
+    }
+    return 0;
+}
