5f204c2a01937a1505ce8d9f8de9d7bbf780cdaa
Sonam <sonurauniyar2062@gmail.com>
2025-11-21 19:42:25 +0545

    assignment completed

--- src/q16.c
+++ src/q16.c
@@ -1,1 +1,32 @@
 // Write a function named reverseArray that takes an array of integers as input and reverses the order of the elements in the array.
+#include <stdio.h>
+
+void reverseArray(int arr[], int size) 
+{
+    for (int i = 0; i < size / 2; i++) 
+    {
+        int temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
+    }
+}
+int main() 
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+    reverseArray(arr, n);
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
