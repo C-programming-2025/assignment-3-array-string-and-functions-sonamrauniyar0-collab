6a3d0b3a0186afc65817e3259711772ec88ae777
Sonam <sonurauniyar2062@gmail.com>
2025-11-21 18:42:16 +0545

    assignment completed

--- src/q3.c
+++ src/q3.c
@@ -1,1 +1,29 @@
 // Write a C program to sort an array of integers in ascending order using a sorting algorithm of your choice (e.g., bubble sort, selection sort, insertion sort).
+#include <stdio.h>
+
+int main() 
+{
+    int n, i, j, a[50], temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n-1; i++)
+        for(j = 0; j < n-i-1; j++)
+            if(a[j] > a[j+1]) 
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+
+    printf("Sorted array: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
