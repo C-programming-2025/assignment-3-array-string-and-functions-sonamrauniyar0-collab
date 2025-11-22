01e76a62a9d9fcba22923f45555b74bd88da3536
Sonam <sonurauniyar2062@gmail.com>
2025-11-22 23:09:29 +0545

    assignment completed

--- src/q26.c
+++ src/q26.c
@@ -1,1 +1,19 @@
 // Write a recursive function named reverseString that takes a string as input and returns the reversed string.
+#include <stdio.h>
+
+void reverseString(char str[], int index) {
+    if (str[index] == '\0')   
+        return;
+    reverseString(str, index + 1);  
+    putchar(str[index]);   
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    reverseString(str, 0);
+    return 0;
+}
